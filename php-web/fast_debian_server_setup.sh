07c327a6c99b2b98b0b798fb2a066463922086c1
rayhon <rayhon1014@gmail.com>
2014-06-05 07:59:58 +0530

    wordpress setup changes.

--- php-web/fast_debian_server_setup.sh
+++ php-web/fast_debian_server_setup.sh
@@ -59,10 +59,11 @@ mv wordpress.vcl /etc/varnish/default.vcl
 # ==============================================================
 # get latest wordpress version
 mkdir -p /var/www/$siteName
+mkdir -p /var/www/$siteName/logs
+cd /var/www/$siteName
 wget http://wordpress.org/latest.tar.gz
 tar -zxvf latest.tar.gz
-mv wordpress/. /var/www/$siteName/.
-rm -rf wordpress
+mv wordpress /var/www/$siteName
 rm latest.tar.gz
 # set up mysql db for wordpress
 echo "CREATE DATABASE IF NOT EXISTS wordpress;GRANT ALL PRIVILEGES ON wordpress.* TO admin@localhost IDENTIFIED BY 'pass' WITH GRANT OPTION;FLUSH PRIVILEGES;" | mysql -u root
