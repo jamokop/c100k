971958ee70b32e41b2b159e937857d14200e45f9
rayhon <ray@MacBook-Pro-1234.local>
2014-06-10 00:14:12 -0700

    fix the folder creation issue for wordpress

--- php-web/fast_debian_server_setup.sh
+++ php-web/fast_debian_server_setup.sh
@@ -70,11 +70,12 @@ mv varnish.txt /etc/default/varnish
 # get latest wordpress version
 #mkdir -p /var/www/$siteName
 #cd /var/www/$siteName
+mkdir -p /var/www/$siteName/logs
 cd /var/www/
 wget http://wordpress.org/latest.tar.gz
 tar -zxvf latest.tar.gz
-mv wordpress /var/www/$siteName
 mkdir -p /var/www/$siteName/logs
+mv wordpress /var/www/$siteName
 rm latest.tar.gz
 # set up mysql db for wordpress
 echo "CREATE DATABASE IF NOT EXISTS wordpress;GRANT ALL PRIVILEGES ON wordpress.* TO admin@localhost IDENTIFIED BY 'pass' WITH GRANT OPTION;FLUSH PRIVILEGES;" | mysql -u root
